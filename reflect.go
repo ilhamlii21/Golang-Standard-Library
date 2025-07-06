8ae4d17c22f2465f34d5e721e4c80af4d805fbbb
williamJmoriarty <140976914+ilhamlii21@users.noreply.github.com>
2025-07-06 23:17:30 +0700

    Enhance struct field definitions in Person and Sample types with validation tags and update readField function to display these tags

--- reflect.go
+++ reflect.go
@@ -6,11 +6,13 @@ import (
 )
 
 type Person struct {
-	Name string
+	Name    string `required:"true" max:"10"`
+	Address string `required:"true" max:"20"`
+	Email   string `required:"true" max:"30"`
 }
 
 type Sample struct {
-	Name string
+	Name string `required:"true" max:"10"`
 }
 
 func readField(value any) {
@@ -18,13 +20,14 @@ func readField(value any) {
 
 	fmt.Println("Type Name: ", valueType.Name())
 	for i := 0; i < valueType.NumField(); i++ {
-		var valueField reflect.StructField = valueType.Field(i)
-		fmt.Println(valueField.Name, "with type: ", valueField.Type)
+		var structField reflect.StructField = valueType.Field(i)
+		fmt.Println(structField.Name, "with type: ", structField.Type)
+		fmt.Println("  required:", structField.Tag.Get("required"))
+		fmt.Println("  max:", structField.Tag.Get("max"))
 	}
-
 }
 
 func main() {
 	readField(Sample{"ilham"})
-	readField(Person{"ilham"})
+	readField(Person{"ilham", "bandung", "ilham@gmail.com"})
 }
